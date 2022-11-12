55904136cce4a04617865b5987c0278516c7ecbf
Yeison <yha.big@gmail.com>
2022-11-11 23:10:33 -0500

    Se crea componente para mostrar las redes sociales en el header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./header.css";
+import HeaderSocial from "./HeaderSocial";
+
 import CTA from "./CTA";
+import ME from "../../assets/me.png";
 
 const Header = () => {
     return (
@@ -10,6 +13,15 @@ const Header = () => {
                 <h1>Hajia Bintu</h1>
                 <h5 className="text-light">Fullstack Developer</h5>
                 <CTA />
+                <HeaderSocial />
+
+                <div className="me">
+                    <img src={ME} alt="me" />
+                </div>
+
+                <a href="#contact" className="scroll__down">
+                    Scroll Down
+                </a>
             </div>
         </header>
     );
