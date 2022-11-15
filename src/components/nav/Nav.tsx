57ddfecc2da6390c9ba0ef6ef1fec8338e4b4b7b
Yeison <yha.big@gmail.com>
2022-11-14 23:47:07 -0500

    Se agregan las secciones de cada item del portafolio

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/Bi";
+import { RiServiceLine } from "react-icons/Ri";
+import { BiMessageSquareDetail } from "react-icons/Bi";
+
 import "./nav.css";
 
 const Nav = () => {
-    return <div>Nav</div>;
+    return (
+        <nav>
+            <a href="#">
+                <AiOutlineHome />
+            </a>
+            <a href="#about">
+                <AiOutlineUser />
+            </a>
+            <a href="#experience">
+                <BiBook />
+            </a>
+            <a href="#services">
+                <RiServiceLine />
+            </a>
+            <a href="#contact">
+                <BiMessageSquareDetail />
+            </a>
+        </nav>
+    );
 };
 
 export default Nav;
